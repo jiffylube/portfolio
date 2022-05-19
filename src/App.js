e589962dd85da6f080cf24b3ec1cfb3d2e4f24fc
jiffylube <lu.jeffrey91@gmail.com>
2022-05-18 22:31:18 -0400

    cleaned up css

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
   return (
     <div className="App">
       <Navbar>
-        <NavItem icon="▼">
+        <NavItem icon="⍱">
           <DropdownMenu
             clickAboutMe={clickAboutMe}
             clickProjects={clickProjects}
