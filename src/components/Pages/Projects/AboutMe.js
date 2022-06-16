370237ec68680995c13cf402e0d700aff9d79be2
jiffylube <lu.jeffrey91@gmail.com>
2022-06-16 16:52:24 -0400

    about me neomorphic shadow

--- src/components/Pages/Projects/AboutMe.js
+++ src/components/Pages/Projects/AboutMe.js
@@ -6,9 +6,11 @@ function AboutMe(props) {
   return (
     <div ref={AboutMeRef} className="pages" id="aboutMe">
       <div className="aboutMeText">
-        <h2>Hi I'm Jeff</h2>
-        <p>I am a Full-Stack developer with a strong background in shipping and logistics. I am an avid athlete, and enjoy passing time taking part in sports and activities. Born and raised in Brooklyn, I still find excitement in exploring the nooks and crannies of NYC and its ever changing communities. This career switch is a huge pivot in my life, and I welcome the challenges along with the creative freedom that it brings. Check out my projects.
-        </p>
+        <div className="boxBorder">
+          <h2>Hi I'm Jeff</h2>
+          <p>I am a Full-Stack developer with a strong background in shipping and logistics. I am an avid athlete, and enjoy passing time taking part in sports and activities. Born and raised in Brooklyn, I still find excitement in exploring the nooks and crannies of NYC and its ever changing communities. This career switch is a huge pivot in my life, and I welcome the challenges along with the creative freedom that it brings. Check out my projects.
+          </p>
+        </div>
       </div>
 
       <div className="imageBox">
